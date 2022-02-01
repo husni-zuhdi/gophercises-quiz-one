fb8315e6cc539c4faf132ea2b7c2d985f8909c8a
Husni Zuhdi <hazunanafaru@gmail.com>
2022-02-01 21:25:55 +0700

    Iterate all problems by line

--- main.go
+++ main.go
@@ -27,11 +27,37 @@ func main() {
 	if err != nil {
 		exit("Failed to load the csv file")
 	}
-	fmt.Println(lines)
+
+	// Parse lines in csv file
+	problems := parseLines(lines)
+
+	// Iterate the problem
+	for i, prob := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, prob.q)
+	}
 }
 
 // exit to print exit message when something gone wrong.
 func exit(msg string) {
 	fmt.Println(msg)
 	os.Exit(1)
+}
+
+// Breakdown the read lines into a new type of struct
+type problem struct {
+	q string
+	a string
+}
+
+// parseLines to parse the csv file into question and answer
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines)) // Prepare variable ret to be returned
+	for i, line := range lines {
+		// Insert problem type struct into ret[i]
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
 }
